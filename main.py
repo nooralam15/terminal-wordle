fdb31d3327b553895fd644a7eb5ccd95b01fcc17
nooralam15 <nooralam14@icloud.com>
2022-06-16 12:24:44 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,53 @@
 #Terminal Based Wordle
 
 #Import libraries
-import random, sys, colorama 
+import random, sys, re, colorama 
 from colorama import Fore
-print(Fore.RED + 'This text is red in color')
+
+#Create a user input processing function
+def userInptProcess(userInput):
+    return list(userInput)
+
+
+
+#create an answer check function
+def answerCheck(answer, userInp, userInpList):
+    for i in range(len(userInpList)):
+        if userInpList[i] == answer[i]:
+            print(Fore.GREEN + userInp[i], end="")
+        elif userInpList[i] in answer:
+            print(Fore.YELLOW + userInpList[i], end="")
+        else:
+            print(Fore.WHITE + userInpList[i], end="")
+    
+    if userInp == answer:
+        print("\n" + "correct guess")
+        
+
+#Function that loads data
+def loadData(data):
+    # Read file as a string
+    fileref = open(data, "r")
+    textData = fileref.read()
+    fileref.close()
+    # Split text by one or more whitespace characters
+    return re.split('\n', textData)
+
+
+def main():
+    #Variable that stores answer list
+    answerList = loadData("database.txt")
+    #stores current
+    currentAnswer = random.choice(answerList)
+    print(currentAnswer)
+
+    
+
+    userInput = input("Enter a five letter word: ").lower()
+    inputArray = userInptProcess(userInput)
+    
+    answerCheck(currentAnswer, userInput, inputArray)
+
+    
+
+main()
